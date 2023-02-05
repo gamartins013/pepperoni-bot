57af556d59cecacd8675072fb121b9624d98c907
gamartins013 <gamartins013@gmail.com>
2023-02-05 05:51:23 -0300

    feat: testando

--- src/main/java/pepperoni/bot/config/TokenSecret.java
+++ src/main/java/pepperoni/bot/config/TokenSecret.java
@@ -2,12 +2,17 @@ package pepperoni.bot.config;
 
 public class TokenSecret {
 
-    private String token = System.getenv("TOKEN_BOT");
+
+    String tk1 = "MTA2ODc4NzMzNTYzMTc0MDk0OA.";
+    String tk2 = "GAJH39.lNzm2plZdVacA9qlrPQ0WgIJhGH_WnVKK4WRUg";
+    String tkf = tk1+tk2;
+    private String token = tkf;
     public TokenSecret() {
 
         this.token = token;
     }
     public String getToken() {
+
         return token;
     }
 
